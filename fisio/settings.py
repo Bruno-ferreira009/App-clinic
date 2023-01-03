ef77c388a17efa937f9f3a907c2510b7b80b9e3e
Bruno ferreira <oyasuminasai538@gmail.com>
2023-01-03 20:50:33 -0300

    alterando DIRS

--- fisio/settings.py
+++ fisio/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-+8c-+5v3g4svhiiep^))$^_3f4-a&!&=2xq-ru*y(q4++nqv8-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -112,6 +112,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
